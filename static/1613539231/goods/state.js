deab85312fbcf2fd7c716fca21b72d07ce501b79
김종혁 <actionbeem929@gmail.com>
2021-02-17 14:21:19 +0900

    build publicPath 추가

---
+++ static/1613539231/goods/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"https:\u002F\u002Factionbeem.github.io\u002Fprod\u002Fstatic\u002F1613539231",layout:"default",error:null,serverRendered:true,routePath:"\u002Fgoods",config:{app:{basePath:"\u002Fprod\u002F",assetsPath:"\u002F",cdnURL:"https:\u002F\u002Factionbeem.github.io\u002Fprod\u002F"}}};
---
+++ static/1613539231/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Factionbeem.github.io\u002Fprod\u002Fstatic\u002F1613539231",layout:"default",error:null,serverRendered:true,routePath:a,config:{app:{basePath:"\u002Fprod\u002F",assetsPath:a,cdnURL:"https:\u002F\u002Factionbeem.github.io\u002Fprod\u002F"}}}}("\u002F"));
